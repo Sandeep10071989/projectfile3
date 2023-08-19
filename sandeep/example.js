bf3b9e03b1f74aab9e6efb6870eaae6d66c51732
Sandeep Mishra <sandeepmishra912@gmail.com>
2023-08-19 04:40:51 -0700

    daaa

---
+++ sandeep/example.js
@@ -0,0 +1,13 @@
+const uid ="abc123";
+uid="abcd234"
+var fullname="Sandeep Mishra";
+var email="sandeepmishra912@gmail.com";
+var password= "123456";
+confirmePassword="123456";
+var courseCount=0;
+var isloggedinFromGoogle=false;
+//fullname=prompt("Enter your name ");
+console.log(uid);
+console.log()
+displaydiscountpercent =Math
+)
